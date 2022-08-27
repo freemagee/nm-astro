88ce1a29fd14b97687bc885fcb4e2931c9e61724
freemagee <1529405+freemagee@users.noreply.github.com>
2022-08-27 18:19:49 +0100

    wip: test out tailwind basic classes

--- src/components/HomeLatestPost.tsx
+++ src/components/HomeLatestPost.tsx
@@ -5,14 +5,14 @@ const HomeLatestPost: FC<IPostProps> = ({ post }) => {
   const { title, date, url } = post;
 
   return (
-    <>
+    <div className="p-2 bg-white border-solid border-t-2 border-blue-500">
       <p>
         {title} on {date.toDateString()}
       </p>
       <p>
         <a href={url}>Read more</a>
       </p>
-    </>
+    </div>
   );
 };
 
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,9 @@
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,svelte,ts,tsx,vue}"],
+  theme: {
+    fontFamily: {
+      sans: ["Montserrat", "sans-serif"],
+    },
+  },
+  plugins: [],
+};
